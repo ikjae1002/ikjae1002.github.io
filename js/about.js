d8f98877acfaf7097624215019ba3fc8ecce3e25
ikjae1002 <ij374@nyu.edu>
2018-10-14 21:46:29 -0400

    Testing for aboutMobile.css

--- js/about.js
+++ js/about.js
@@ -2,7 +2,6 @@
 AOS.init();
 var myname = document.querySelector("h1");
 var navigate = document.querySelectorAll("li");
-var underline = document.getElementById("active");
 var time = 1000;
 
 function changeProperty(ele){
@@ -10,8 +9,8 @@ function changeProperty(ele){
 }
 
 setTimeout(function(e){
-    e.style.width = "50%";
-},2400,underline);
+    $('#active').animate({marginTop: "5px"}, 1000);
+},2000);
 
 setTimeout(changeProperty,500,myname);
 
@@ -20,19 +19,73 @@ for(var i = 0; i<navigate.length; i++){
     time += 400;
 }
 
+for(var i = 0; i < 11; i++){
+    if($('div').hasClass(i.toString())){
+        console.log($('.' + i.toString()));
+        for(var j = 0; j < 10 - i; j++){
+            var appendClass = $('<div></div>').addClass("empty");
+            $('.' + i.toString()).append(appendClass);
+        }for(var j = 0; j < i; j++){
+            var appendClass = $('<div></div>').addClass("fill");
+            $('.' + i.toString()).append(appendClass);
+        }
+    }
+}
+
 //jquery for CSS
 
-$(document).on('click', "a", function(){
-    var newurl = $(this).attr('href');
+$('#screen').animate({height: '0px'}, 1000);
 
-    if(newurl == '#'){
-        location.hash = newurl;
-        return;
-    }
+$('#one').click(function (e) {
+    e.preventDefault();
+    var goTo = "home.html";
 
-    $("html").fadeOut(function(){
-        location = newurl;
-    });
-
-    return false;
+    $('#screen').css({'background-color': '#d0b783','z-index': '5', 'margin-top': '-800px', 'overflow': 'visible'});
+    $('#screen').animate({height: '2000px'}, 3000);
+    $('#title').css({'text-align': 'center', 'top': '0', 'height': '100vh', 'position': 'absolute', 'z-index': '6'});
+    $('h2').text('Home');
+    $('h2').css({'opacity': '0', 'color': 'black', 'font-family': 'Raleway, Helvetica', 'font-size': '3em', 'font-weight': '300', 'top': '50%','left': '50%', 'transform': 'translate(-50%, -50%)', 'position': 'absolute', 'margin': '0'});
+    setTimeout(function() {
+        $('h2').animate({opacity: '1'}, 1000);
+        setTimeout(function(){
+            $('h2').animate({opacity: '0'}, 1000);
+        }, 1000);
+    }, 2000);
+    setTimeout(function(){
+        window.location = goTo;
+    }, 4000);
 });
+
+$('#three').click(function (e) {
+    e.preventDefault();
+    var goTo = "photos.html";
+
+    $('#screen').css({'background-color': '#bcd2d0','z-index': '5', 'margin-top': '-800px', 'overflow': 'visible'});
+    $('#screen').animate({height: '2000px'}, 3000);
+    $('#title').css({'text-align': 'center', 'top': '0', 'height': '100vh', 'position': 'absolute', 'z-index': '6'});
+    $('h2').text('Photos');
+    $('h2').css({'opacity': '0', 'color': 'black', 'font-family': 'Raleway, Helvetica', 'font-size': '3em', 'font-weight': '300', 'top': '50%','left': '50%', 'transform': 'translate(-50%, -50%)', 'position': 'absolute', 'margin': '0'});
+    setTimeout(function() {
+        $('h2').animate({opacity: '1'}, 1000);
+        setTimeout(function(){
+            $('h2').animate({opacity: '0'}, 1000);
+        }, 1000);
+    }, 2000);
+    setTimeout(function(){
+        window.location = goTo;
+    }, 4000);
+});
+
+$(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() + 30 > parseInt($(this).height())){
+            $('a').css({'color':'black'});
+            $('nav').css({'background-color': 'white'});
+        }
+        if ($(this).scrollTop() + 30 < parseInt($(this).height())){
+            $('a').css({'color':'white'});
+            $('nav').css({'background-color': 'transparent'});
+        }
+    });
+});
+
--- js/home.js
+++ js/home.js
@@ -21,21 +21,6 @@ for(var i = 0; i<navigate.length; i++){
 
 //jquery for CSS
 
-$(document).on('click', "a", function(){
-    var newurl = $(this).attr('href');
-
-    if(newurl == '#'){
-        location.hash = newurl;
-        return;
-    }
-
-    $("html").fadeOut(function(){
-        location = newurl;
-    });
-
-    return false;
-});
-
 $('#screen').animate({height: '0px'}, 1000);
 
 setTimeout(function(){
@@ -53,3 +38,43 @@ setTimeout(function(){
     $('#thirdleft').animate({left: '0px'}, 1000);
     $('#thirdright').animate({right: '0px'}, 1000);
 },2000);
+
+$('#two').click(function (e) {
+    e.preventDefault();
+    var goTo = "about.html";
+
+    $('#screen').css({'background-color': '#f96161','z-index': '5', 'margin-top': '-800px', 'overflow': 'visible'});
+    $('#screen').animate({height: '2000px'}, 3000);
+    $('#title').css({'text-align': 'center', 'top': '0', 'height': '100vh', 'position': 'absolute', 'z-index': '6'});
+    $('h2').text('About');
+    $('h2').css({'opacity': '0', 'color': 'black', 'font-family': 'Raleway, Helvetica', 'font-size': '3em', 'font-weight': '300', 'top': '50%','left': '50%', 'transform': 'translate(-50%, -50%)', 'position': 'absolute', 'margin': '0'});
+    setTimeout(function() {
+        $('h2').animate({opacity: '1'}, 1000);
+        setTimeout(function(){
+            $('h2').animate({opacity: '0'}, 1000);
+        }, 1000);
+    }, 2000);
+    setTimeout(function(){
+        window.location = goTo;
+    }, 4000);
+});
+
+$('#three').click(function (e) {
+    e.preventDefault();
+    var goTo = "photos.html";
+
+    $('#screen').css({'background-color': '#bcd2d0','z-index': '5', 'margin-top': '-800px', 'overflow': 'visible'});
+    $('#screen').animate({height: '2000px'}, 3000);
+    $('#title').css({'text-align': 'center', 'top': '0', 'height': '100vh', 'position': 'absolute', 'z-index': '6'});
+    $('h2').text('Photos');
+    $('h2').css({'opacity': '0', 'color': 'black', 'font-family': 'Raleway, Helvetica', 'font-size': '3em', 'font-weight': '300', 'top': '50%','left': '50%', 'transform': 'translate(-50%, -50%)', 'position': 'absolute', 'margin': '0'});
+    setTimeout(function() {
+        $('h2').animate({opacity: '1'}, 1000);
+        setTimeout(function(){
+            $('h2').animate({opacity: '0'}, 1000);
+        }, 1000);
+    }, 2000);
+    setTimeout(function(){
+        window.location = goTo;
+    }, 4000);
+});
